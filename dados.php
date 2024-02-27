6836e75bc2aefbbfdee6dd8d146ee3dc7ba92bc5
Nikolas Railan <nikolasrailanmp@gmail.com>
2024-02-27 12:32:31 -0300

    .

---
+++ dados.php
@@ -0,0 +1,7 @@
+<?php
+$nomeSite = "Arlindo";
+$nomeProduto = array("sal", "agua", "trigo", "maizena");
+
+echo $nomeSite;
+echo "<br>";
+?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Exemplo</title>
+    <title>$nomeSite</title>
 </head>
 <body>
     <?php include "dados.php"; ?>
